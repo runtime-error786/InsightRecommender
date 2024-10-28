2cd546a78fb96b818d6632c20df84ea0f0cdae9b
Mustafa Rizwan <mustafa782a@gmail.com>
2024-10-28 20:18:01 +0500

    use dataset from amazon

--- Storedata_&_embedding/mongo_pinecone_integration.py
+++ Storedata_&_embedding/mongo_pinecone_integration.py
@@ -2,18 +2,39 @@ from transformers import AutoTokenizer, AutoModel
 import torch
 from setup_mogodb import collection
 from setup_pinecone import index
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 tokenizer = AutoTokenizer.from_pretrained("sentence-transformers/all-MiniLM-L6-v2")
 model = AutoModel.from_pretrained("sentence-transformers/all-MiniLM-L6-v2")
 
-def generate_embedding(text):
-    tokens = tokenizer(text, return_tensors="pt", padding=True, truncation=True)
+def generate_embedding(product):
+    """Generate embedding for product details (Name, Description, Tags)."""
+    combined_text = f"{product['Product Title']} {product['Product Description']} {product['Brand']} {product['Site Name']} {product['Category']}"
+    tokens = tokenizer(combined_text, return_tensors="pt", padding=True, truncation=True)
     with torch.no_grad():
         embedding = model(**tokens).last_hidden_state.mean(dim=1).squeeze()
-    return embedding.numpy()
+    return str(product["Uniq Id"]), embedding.numpy()
 
-for product in collection.find():
-    embedding = generate_embedding(product["Description"])
-    index.upsert([(str(product["Product_ID"]), embedding)])
+def upload_to_pinecone(product_embedding):
+    """Upload the embedding to Pinecone."""
+    uniq_id, embedding = product_embedding
+    index.upsert([(uniq_id, embedding)])
+
+def main():
+    futures = []
     
-print("Embeddings uploaded to Pinecone.")
+    with ThreadPoolExecutor(max_workers=100) as executor:  
+        for product in collection.find():
+            futures.append(executor.submit(generate_embedding, product))
+        
+        for future in as_completed(futures):
+            try:
+                product_embedding = future.result()
+                upload_to_pinecone(product_embedding)
+            except Exception as e:
+                print(f"Error processing product: {e}")
+
+    print("Embeddings uploaded to Pinecone.")
+
+if __name__ == "__main__":
+    main()
--- Storedata_&_embedding/setup_mogodb.py
+++ Storedata_&_embedding/setup_mogodb.py
@@ -1,11 +1,20 @@
+import os
 import pandas as pd
 from pymongo import MongoClient
+from datasets import load_dataset
+from transformers import AutoTokenizer, AutoModel
+import torch
+from dotenv import load_dotenv
+from pinecone import Pinecone, ServerlessSpec
 
-df = pd.read_excel("../dataset/sample_products_dataset.xlsx")
+load_dotenv()
 
 client = MongoClient("mongodb://localhost:27017/")
 db = client["recommender_db"]
 collection = db["products"]
 
+dataset = load_dataset("pgurazada1/amazon_india_products")
+
+df = pd.DataFrame(dataset['train']) 
 collection.insert_many(df.to_dict(orient="records"))
 print("Data successfully uploaded to MongoDB.")
